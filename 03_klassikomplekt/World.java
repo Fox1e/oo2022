ba6d29896b3a5bf4c855633ee04fde3ad089a71a
Fox1e <foxie@tlu.ee>
2022-05-25 15:02:08 +0300

    'OOP'

---
+++ 03_klassikomplekt/World.java
@@ -0,0 +1,54 @@
+
+import java.util.List;
+
+public class World {
+    int height;
+    int width;
+    List<Item> items;
+
+    public World() {
+        this.height = height;
+        this.width = width;
+
+    }
+
+
+    public void printMap(int playerCoordinateY, int playerCoordinateX,
+                         int enemyCoordinateY, int enemyCoordianteX) {
+        // fori --> pakub
+        char symbol;   // deklareerib 1x muutuja, mitte iga kord deklareerib selle muutuja
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (y == 0 || y == height-1) { // primitiiv || andmebaasipäring
+                    symbol = '-';
+                } else if (x == 0 || x == width-1) {
+        for (y = 0; y < height; y++) {
+            for (x = 0; x < width; x++) {
+                if (y == 0 || y == height -1) { // primitiiv || andmebaasipäring
+                    symbol = '-';
+                } else if (x == 0 || x == width -1) {
+                    symbol = '|';
+                } else {
+                    symbol = ' ';
+                }
+                for (int i = 0; i < items.size(); i++) {
+                    if(items.get(i).coordinateY == y && items.get(i).coordinateX == x){
+                        symbol = 'I';
+                        break;
+                    }
+                }
+                if (playerCoordinateY == y && playerCoordinateX == x) {
+                    symbol = 'x';
+                }
+                if (enemyCoordinateY == y && enemyCoordianteX == x) {
+                    symbol = 'z';
+                }
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+}}}
